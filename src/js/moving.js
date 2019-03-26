caf0857b558f8744b0cc9ac40142caf3c242692c
Tu Doan Minh <dmtu@apcs.vn>
2019-03-26 20:34:00 +0700

    Fix syntax and add Level 1

--- src/js/moving.js
+++ src/js/moving.js
@@ -1,35 +1,41 @@
-function goDown(map,pacman,callBack){
-    if ( map[pacman.y+1][pacman.x] !== 1){
-        map[pacman.y][pacman.x] = 0;
-        pacman.y = pacman.y + 1;
-        map[pacman.y][pacman.x] = 5;
-        callBack(map);
+function canMove(map, i, j) {
+    if (map[i][j] == 0 || map[i][j] == 2)
+        return true;
+    return false;
+}
+
+function goDown(map, pacman){
+    if (canMove(map, pacman.i+1, pacman.j)) {
+        map[pacman.i][pacman.j] = 0;
+        pacman.i = pacman.i + 1;
+        map[pacman.i][pacman.j] = 5;
+        drawWorld(map)
     }
 }
 
-function goUp(map,pacman,callBack){
-    if ( map[pacman.y-1][pacman.x] !== 1){
-        map[pacman.y][pacman.x] = 0;
-        pacman.y = pacman.y - 1;
-        map[pacman.y][pacman.x] = 5;
-        callBack(map);
+function goUp(map, pacman){
+    if (canMove(map, pacman.i-1, pacman.j)) {
+        map[pacman.i][pacman.j] = 0;
+        pacman.i = pacman.i - 1;
+        map[pacman.i][pacman.j] = 5;
+        drawWorld(map)
     }
 }
 
-function goLeft(map,pacman,callBack){
-    if ( map[pacman.y][pacman.x-1] !== 1){
-        map[pacman.y][pacman.x] = 0;
-        pacman.x = pacman.x - 1;
-        map[pacman.y][pacman.x] = 5;
-        callBack(map);
+function goLeft(map, pacman){
+    if (canMove(map, pacman.i, pacman.j-1)) {
+        map[pacman.i][pacman.j] = 0;
+        pacman.j = pacman.j - 1;
+        map[pacman.i][pacman.j] = 5;
+        drawWorld(map)
     }
 }
 
-function goRight(map,pacman,callBack){
-    if ( map[pacman.y][pacman.x+1] !== 1){
-        map[pacman.y][pacman.x] = 0;
-        pacman.x = pacman.x + 1;
-        map[pacman.y][pacman.x] = 5;
-        callBack(map);
+function goRight(map, pacman){
+    if (canMove(map, pacman.i, pacman.j+1)){
+        map[pacman.i][pacman.j] = 0;
+        pacman.j = pacman.j + 1;
+        map[pacman.i][pacman.j] = 5;
+        drawWorld(map)
     }
 }
